e97ac4395f21daf8cad3bc9ec8a9fc0d07d0580e
Ashik-AD <looge321@gmail.com>
2023-04-08 23:01:37 +0545

    add db schema

---
+++ store/store.sql
@@ -0,0 +1,21 @@
+
+CREATE DATE student;
+
+use student;
+
+CREATE  TABLE registered_student (
+    id INT(20) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(100) NOT NULL,
+    mid_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    father_name VARCHAR(250) NOT NULL,
+    mother_name VARCHAR(250) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    dob VARCHAR(20) NOT NULL,
+    gender VARCHAR(20) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    contact_no VARCHAR(20) NOT NULL,
+    parent_contact_no VARCHAR(20) NOT NULL,
+    applied_course VARCHAR(20) NOT NULL,
+);
+
